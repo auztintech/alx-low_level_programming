8bed7d216eecc8658ff8367d426e67691234ca38
auztintech <auztintech@gmail.com>
2022-09-08 09:21:58 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *main -main block
  *Return: 0
@@ -7,7 +6,7 @@
 
 int main(void) 
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
