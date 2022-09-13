db5ded370e5a3ddf84e3c8721bffad1139b7d41f
auztintech <auztintech@gmail.com>
2022-09-13 08:14:29 -0700

    4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - Return 1 when the letter entered is lowercase or 0 if isn't.
+ * Return: 1 for lowercase, 0 for if not
+ */
+int _islower(int b)
+{
+	if (b > 96 && b < 123)
+		return (1);
+	else
+		return (0);
+}
