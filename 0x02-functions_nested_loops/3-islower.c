b31282655ec61e045e2ed148a3a996eab452c232
auztintech <auztintech@gmail.com>
2022-09-13 08:25:33 -0700

    4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - Return 1 when the letter entered is lowercase or 0 if isn't.
+ * @b: char type letter
  * Return: 1 for lowercase, 0 for if not
  */
 int _islower(int b)
