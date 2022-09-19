6e0e91819b15e135d053fd5dd16afb87762870a0
auztintech <auztintech@gmail.com>
2022-09-19 09:28:24 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _str_length - returns the length of a string
+ * @s: character
+ * Return: i
+ */
+int_strlen(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		
+	}
+	return (n);
+}
