38eafd23372cda8d1ccd646cbbd121cc136f4269
auztintech <auztintech@gmail.com>
2022-09-19 12:31:23 -0700

    task 9

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
 void swap_int(int *a, int *b);
 
