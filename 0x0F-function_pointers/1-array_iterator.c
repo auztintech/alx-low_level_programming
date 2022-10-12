0c47c0a4fb232c2fb5135a4dc2314acee5551252
auztintech <auztintech@gmail.com>
2022-10-12 14:49:16 -0700

    function that executes a function given as paremeter on elements

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int n;
 
-	if (array == NULL || action == NUL)
+	if (array == NULL || action == NULL)
 		return;
 	for (n = 0; n < size; n++)
 		action(array[n]);
